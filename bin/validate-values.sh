22d2add7ce6df7e1b71e16354f630c7b8aabbac6
Christos Spiliopoulos <cspiliopoulos@atypon.com>
2022-03-22 13:33:27 +0200

    more cleanup

--- bin/validate-values.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-
-./bin/validate-values.js \
-  ${DIR}/../docker/values.yml \
-  ${DIR}/../helm_repo/manuscripts-api/values.yaml \
-  ${DIR}/../helm_repo/manuscripts-api/charts/**/values.yaml
