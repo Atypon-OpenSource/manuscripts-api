3c30fced3c6a4f5161c8b1bf02f5f7f01d9ffb70
Christos Spiliopoulos <cspiliopoulos@atypon.com>
2022-06-27 16:26:40 +0300

    fix seeding

--- bin/seed-app.sh
+++ bin/seed-app.sh
@@ -44,7 +44,7 @@ do
   -H 'manuscripts-app-secret: Valid secret' \
   -H "Authorization: Bearer $token" \
   --data-binary "{\"_id\":\"MPProject:$user\"}" && \
-  echo -e "Project\"MPProject:$user\" created\n"
+  echo -e "\"MPProject:$user\" created\n"
 
   curl  -X "POST" "http://localhost:3000/api/v1/container/projects/MPProject:$user/manuscripts/MPManuscript:$user" \
   --fail \
@@ -53,12 +53,18 @@ do
   -H 'manuscripts-app-id: io.manuscripts' \
   -H 'manuscripts-app-secret: Valid secret' \
   -H "Authorization: Bearer $token" && \
-  echo -e "\"Manuscript:$user\" created\n"
+  echo -e "\"MPManuscript:$user\" created\n"
 
 
   parent_path=$(pwd)
   zip_path="test/data/fixtures/sample/seed.manuscript-json"
+  user_specific_zip_path="test/data/fixtures/sample/$user.manuscript-json"
   file_path="$parent_path/$zip_path"
+  user_specific_file_path="$parent_path/$user_specific_zip_path"
+
+  cp $file_path $user_specific_file_path
+  sed -i '' "s/MPProject:B81D5F33-6338-420C-AAEC-CF0CF33E675C/MPProject:$user/g" $user_specific_file_path
+  sed -i '' "s/MPManuscript:9E0BEDBC-1084-4AA1-AB82-10ACFAE02232/MPManuscript:$user/g" $user_specific_file_path
 
   curl "http://localhost:3000/api/v1/project/MPProject:$user/save" \
   --fail \
@@ -67,7 +73,9 @@ do
   -H 'manuscripts-app-id: io.manuscripts' \
   -H 'manuscripts-app-secret: Valid secret' \
   -H "Authorization: Bearer $token" \
-  --data "@$file_path" \
+  --data "@$user_specific_file_path" \
+
+  rm $user_specific_file_path
 
   echo -e "Project\"MPProject:$user\" seeded with a manuscript\n"
 
