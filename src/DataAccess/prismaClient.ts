b40d6360a18dd34f300192a28b75995ed68857de
mnatsheh <mnatsheh@atypon.com>
2023-05-31 16:43:12 -0700

    LEAN-2540 - add log messages

--- src/DataAccess/prismaClient.ts
+++ src/DataAccess/prismaClient.ts
@@ -16,6 +16,8 @@
 
 import { PrismaClient } from '@prisma/client'
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient({
+  log: ['query', 'info', 'warn', 'error'],
+})
 
 export default prisma
