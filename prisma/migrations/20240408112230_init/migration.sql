47cfec906a55960ecaff39fa1189cab314b7cc9d
Mohammad Betamony <mbetamony@atypon.com>
2024-04-08 14:22:38 +0300

    wip

--- prisma/migrations/20240408112230_init/migration.sql
+++ prisma/migrations/20240408112230_init/migration.sql
@@ -1,9 +1,13 @@
 /*
   Warnings:
 
+  - You are about to drop the `ManuscriptDocHistory` table. If the table is not empty, all the data it contains will be lost.
   - Made the column `version` on table `ManuscriptDoc` required. This step will fail if there are existing NULL values in that column.
 
 */
 -- AlterTable
 ALTER TABLE "ManuscriptDoc" ADD COLUMN     "steps" JSONB[] DEFAULT ARRAY[]::JSONB[],
 ALTER COLUMN "version" SET NOT NULL;
+
+-- DropTable
+DROP TABLE "ManuscriptDocHistory";
