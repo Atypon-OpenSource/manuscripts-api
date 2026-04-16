524275f09029d670a548b80b84aa690dd4cb20cf
mbartenev-atypon <mbartenev@atypon.com>
2026-04-16 17:18:07 +0200

    updates

--- src/DomainServices/UserService.ts
+++ src/DomainServices/UserService.ts
@@ -60,8 +60,6 @@ export class UserService {
   private createUserProfile(user: User): UserProfile {
     return {
       _id: `MPUserProfile:${user.id.replace('User_', '')}`,
-      family: user.family,
-      given: user.given,
       userID: user.id,
       connectID: user.connectUserID
     }
